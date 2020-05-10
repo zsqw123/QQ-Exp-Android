aa7e8982b123e2e57b37e41b7a7f104e1684d4e2
damyj <3206279058@qq.com>
2020-05-10 17:44:37 +0800

    - use root to copy DB - Optimize View

--- app/src/main/java/qhaty/qqex/Ex.kt
+++ app/src/main/java/qhaty/qqex/Ex.kt
@@ -1,9 +1,28 @@
 package qhaty.qqex
 
-class Ex {
-    companion object{
-        suspend fun doEx(){
+import java.io.File
+import java.io.IOException
 
+class Ex {
+    companion object {
+        suspend fun doEx(libFileNew: File, libFileOld: File): File? {
+            return null
         }
     }
+}
+
+@Throws(IOException::class)
+fun initFile(path: String) {
+    val file = File(path)
+    if (!file.parentFile?.exists()!!) {
+        file.parentFile?.mkdirs()
+    }
+    val imgFiles = File(file.parentFile!!.absolutePath+"/images")
+    if (!imgFiles.exists()) {
+        imgFiles.mkdirs()
+    }
+    if (file.exists()) {
+        file.delete()
+    }
+    file.createNewFile()
 }
