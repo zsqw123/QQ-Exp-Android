425b76b10d8882689bc8b6ba726ab913720a2853
damyj <3206279058@qq.com>
2020-05-10 19:26:44 +0800

    - 异步处理database

--- app/src/main/java/qhaty/qqex/Ex.kt
+++ app/src/main/java/qhaty/qqex/Ex.kt
@@ -1,13 +1,50 @@
 package qhaty.qqex
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import kotlinx.coroutines.*
 import java.io.File
 import java.io.IOException
+import java.util.*
+import kotlin.collections.HashMap
 
 class Ex {
-    companion object {
-        suspend fun doEx(libFileNew: File, libFileOld: File): File? {
-            return null
+    private val allChat = arrayListOf<HashMap<String, Any>>()
+    fun startEx(context: Context) {
+        GlobalScope.launch {
+            val dbFileList = GetDB(context).getDataBase()
+            doEx(dbFileList!![0], dbFileList[1])
         }
+    }
+
+    private suspend fun doEx(libFileNew: File, libFileOld: File?): File? {
+        withContext(Dispatchers.IO) {
+            val old = async(Dispatchers.IO) { if (libFileOld != null) addDByPath(libFileOld) }
+            val new = async(Dispatchers.IO) { addDByPath(libFileNew) }
+            old.await()
+            new.await()
+
+        }
+        return null
+    }
+
+    private fun addDByPath(libFile: File) {
+        val oldSql = SQLiteDatabase.openDatabase(libFile.absolutePath, null, SQLiteDatabase.OPEN_READWRITE)
+        val friendOrTroop = if (Data.friendOrGroup) "friend" else "troop"
+        val sqlDo = "SELECT _id,msgData,msgtype,senderuin,time FROM mr_${friendOrTroop}_" +
+                "${encodeMD5(Data.friendQQ).toUpperCase(Locale.ROOT)}_New ORDER BY time ASC;"
+        val cursorOld = oldSql.rawQuery(sqlDo, null)
+        if (cursorOld.count > 1) cursorOld.moveToFirst()
+        do {
+            val single = HashMap<String, Any>()
+            single["sender"] = cursorOld.getString(3)
+            single["type"] = cursorOld.getInt(2)
+            single["data"] = cursorOld.getBlob(1)
+            single["time"] = cursorOld.getInt(4)
+            allChat.add(single)
+        } while (cursorOld.moveToNext())
+        oldSql.close()
+        cursorOld.close()
     }
 }
 
@@ -17,7 +54,7 @@ fun initFile(path: String) {
     if (!file.parentFile?.exists()!!) {
         file.parentFile?.mkdirs()
     }
-    val imgFiles = File(file.parentFile!!.absolutePath+"/images")
+    val imgFiles = File(file.parentFile!!.absolutePath + "/images")
     if (!imgFiles.exists()) {
         imgFiles.mkdirs()
     }
--- app/src/main/java/qhaty/qqex/GetDB.kt
+++ app/src/main/java/qhaty/qqex/GetDB.kt
@@ -2,6 +2,8 @@ package qhaty.qqex
 
 import android.content.Context
 import com.jaredrummler.android.shell.Shell
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
 
 class GetDB(private var context: Context) {
@@ -21,26 +23,27 @@ class GetDB(private var context: Context) {
                 copyUseRoot()
                 listOf(dbFileNew)
             } else {
-                context.toast("无法获取聊天数据文件")
+                withContext(Dispatchers.Main) { context.toast("无法获取聊天数据文件") }
                 null
             }
         } else {
-            context.toast("无内置储存")
+            withContext(Dispatchers.Main) { context.toast("无内置储存") }
             return null
         }
     }
 
     private suspend fun copyUseRoot() {
-        if (Shell.SU.available()) {
-            val dir = context.getExternalFilesDir(null)!!
-            if (!dir.exists()) dir.mkdirs()
-            val qqPkg = "com.tencent.mobileqq"
-            val cmd0 = "am force-stop $qqPkg"
-            val cmd1 =
-                "cp -f /data/data/$qqPkg/databases/${Data.meQQ}.db ${dir.absolutePath}/${Data.meQQ}.db"
-            val cmd2 =
-                "cp -f /data/data/$qqPkg/databases/slowtable_${Data.meQQ}.db ${dir.absolutePath}/slowtable_${Data.meQQ}.db"
-            Shell.SU.run(cmd0, cmd1, cmd2)
+        withContext(Dispatchers.IO) {
+            if (Shell.SU.available()) {
+                val dir = context.getExternalFilesDir(null)!!
+                val qqPkg = "com.tencent.mobileqq"
+                val cmd0 = "am force-stop $qqPkg"
+                val cmd1 =
+                    "cp -f /data/data/$qqPkg/databases/${Data.meQQ}.db ${dir.absolutePath}/${Data.meQQ}.db"
+                val cmd2 =
+                    "cp -f /data/data/$qqPkg/databases/slowtable_${Data.meQQ}.db ${dir.absolutePath}/slowtable_${Data.meQQ}.db"
+                Shell.SU.run(cmd0, cmd1, cmd2)
+            }
         }
     }
 }
