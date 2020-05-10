625f1cb0ca99a3127441f5da7fddbdb3ec5de6c0
damyj <3206279058@qq.com>
2020-05-10 23:02:41 +0800

    - 处理导出

--- app/src/main/java/qhaty/qqex/Utils.kt
+++ app/src/main/java/qhaty/qqex/Utils.kt
@@ -1,7 +1,12 @@
 package qhaty.qqex
 
+import android.content.ContentValues
 import android.content.Context
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
+import java.io.*
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
@@ -30,4 +35,25 @@ fun encodeMD5(text: String): String {
         e.printStackTrace()
     }
     return ""
+}
+
+fun textToDownload(context: Context, fileName: String, text: String) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val values = ContentValues()
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+        values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS + "/QQEX")
+        val uri = context.contentResolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values)
+        if (uri != null) {
+            val outputStream = context.contentResolver.openOutputStream(uri)
+            outputStream?.write(text.toByteArray())
+        }
+    } else {
+        @Suppress("DEPRECATION")
+        val path = "${Environment.getExternalStorageDirectory().absolutePath}/${Environment.DIRECTORY_DOWNLOADS}/QQEX"
+        if (!File(path).exists()) File(path).mkdirs()
+        val file = File("$path/$fileName")
+        if (file.exists()) file.delete()
+        file.createNewFile()
+        FileOutputStream(file).write(text.toByteArray())
+    }
 }
