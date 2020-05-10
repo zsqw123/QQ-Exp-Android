9882cd24bca7c11da1d15c1bc62bd6184e66a280
damyj <3206279058@qq.com>
2020-05-10 19:26:44 +0800

    - 异步处理database

--- app/src/main/java/qhaty/qqex/Utils.kt
+++ app/src/main/java/qhaty/qqex/Utils.kt
@@ -2,7 +2,32 @@ package qhaty.qqex
 
 import android.content.Context
 import android.widget.Toast
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 fun Context.toast(str: String? = null, id: Int? = null) {
     Toast.makeText(this, str ?: this.resources.getText(id!!), Toast.LENGTH_SHORT).show()
+}
+
+fun encodeMD5(text: String): String {
+    try {
+        val instance: MessageDigest = MessageDigest.getInstance("MD5")
+        val digest: ByteArray = instance.digest(text.toByteArray())
+        val sb = StringBuffer()
+        for (b in digest) {
+            //获取低八位有效值
+            val i: Int = b.toInt() and 0xff
+            //将整数转化为16进制
+            var hexString = Integer.toHexString(i)
+            if (hexString.length < 2) {
+                //如果是一位的话，补0
+                hexString = "0$hexString"
+            }
+            sb.append(hexString)
+        }
+        return sb.toString()
+    } catch (e: NoSuchAlgorithmException) {
+        e.printStackTrace()
+    }
+    return ""
 }
