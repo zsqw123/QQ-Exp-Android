6be3d622414f0102686d9ea735803a5eb68e0824
damyj <3206279058@qq.com>
2020-05-12 11:37:06 +0800

    - 统一数据类型 - Fix bugs

--- app/src/main/java/qhaty/qqex/Ex.kt
+++ app/src/main/java/qhaty/qqex/Ex.kt
@@ -1,76 +1,87 @@
 package qhaty.qqex
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.os.Build
 import android.view.View
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.HashMap
 import kotlin.experimental.xor
 import kotlin.properties.Delegates
 
 class Ex {
-    private var htmlStr = "<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /></head>"
     private var saveStr = ""
+    private var html = ""
     private var progress: Progress by Delegates.observable(
         Progress(0, "开始导出...")
     ) { _, _, new -> onProgressChange(new) }
 
     fun startEx(context: Context, keyGenText: String? = null) {
-        GlobalScope.launch(Dispatchers.Default) {
+        GlobalScope.launch {
             progress = Progress(10, "读取数据库...")
-            val dbFileList: List<File>? = GetDB(context).getDataBase()
-            if (dbFileList == null) {
+            val dbFileTask = async { GetDB(context).getDataBase() }
+            if (dbFileTask.await() == null) {
                 progress = Progress(progress.progress, "无法读取数据库\n请手动导入")
                 return@launch
             }
-            progress = Progress(50, "导出数据库...")
-            doEx(dbFileList[0], dbFileList[1], context, keyGenText)
-        }
-    }
 
-    private suspend fun doEx(libFileNew: File, libFileOld: File?, context: Context, keyGenText: String? = null) {
-        withContext(Dispatchers.Default) {
-            val old =
-                withContext(Dispatchers.IO) { if (libFileOld != null) addDByPath(libFileOld) else null }
-            val new = withContext(Dispatchers.IO) { addDByPath(libFileNew, keyGenText) }
-            if (old != null) new += old
+            val dbFileList = dbFileTask.await()!!
+            progress = Progress(50, "导出数据库...")
+            val allChat: ArrayList<CodedChat>
+            val new = async(Dispatchers.IO) { addDByPath(dbFileList[0], keyGenText) }
+            val old = if (dbFileList.size > 1) async(Dispatchers.IO) { addDByPath(dbFileList[1]) } else null
+            val newR = new.await()
+            val oldR = old?.await()
+            when {
+                newR == null -> {
+                    if (oldR == null) {
+                        progress = Progress(1000, "QQ无本地聊天记录")
+                        return@launch
+                    }
+                    allChat = oldR
+                }
+                oldR == null -> allChat = newR
+                else -> allChat = (newR + oldR) as ArrayList<CodedChat>
+            }
+
             progress = Progress(200, "解析数据库...")
-            toHtml(withContext(Dispatchers.Default) { chatsDecode(new) })
+            val decodedChat = chatsDecode(allChat)
             progress = Progress(progress.progress, "保存网页到本地中...")
-            textToDownload(context, Data.friendQQ, htmlStr)
+            toHtml(decodedChat)
+            textToAppDownload(context, Data.friendQQ, html)
             textToAppData(context, Data.friendQQ, saveStr)
             progress = Progress(1000, "保存成功")
         }
     }
 
-    private fun addDByPath(libFile: File, keyGenText: String? = null): ArrayList<HashMap<String, Any>> {
-        val chats = arrayListOf<HashMap<String, Any>>()
+    @SuppressLint("Recycle")
+    private fun addDByPath(libFile: File, keyGenText: String? = null): ArrayList<CodedChat>? {
+        val chats = arrayListOf<CodedChat>()
         val sql = SQLiteDatabase.openDatabase(libFile.absolutePath, null, 0)
         val friendOrTroop = if (Data.friendOrGroup) "friend" else "troop"
         val sqlDo = "SELECT _id,msgData,msgtype,senderuin,time FROM mr_${friendOrTroop}_" +
                 "${encodeMD5(Data.friendQQ).toUpperCase(Locale.ROOT)}_New"
         try {
-            val cursor = sql.rawQuery(sqlDo, null)
+            val cursor = try {
+                sql.rawQuery(sqlDo, null)
+            } catch (e: java.lang.Exception) {
+                null
+            } ?: return null
             if (cursor.count > 1) cursor.moveToFirst()
             var first = true
             val keyGen: Boolean = keyGenText != null
             do {
-                val single = HashMap<String, Any>()
-                single["data"] = cursor.getBlob(1)
-                single["type"] = cursor.getInt(2)
-                single["sender"] = cursor.getString(3)
-                single["time"] = cursor.getInt(4)
-                chats += single
+                val data = cursor.getBlob(1)
+                val type = cursor.getInt(2)
+                val sender = cursor.getString(3)
+                val time = cursor.getInt(4)
+                chats += CodedChat(time, type, sender, data)
                 if (first && keyGen) {
-                    dbLastData = single["data"] as ByteArray
+                    dbLastData = data as ByteArray
                     decodeKey(keyGenText!!)
                     first = false
                 }
@@ -82,41 +93,46 @@ class Ex {
         return chats
     }
 
-    private fun chatsDecode(allChat: ArrayList<HashMap<String, Any>>): ArrayList<HashMap<String, Any>> {
-        val allChatDecode = arrayListOf<HashMap<String, Any>>()
-        val single = hashMapOf<String, Any>()
-        val allCount = allChat.size
-        progress = Progress(progress.progress, "数据库解码...")
-        for (i in allChat.indices) {
-            single["time"] = allChat[i]["time"] as Int
-            single["type"] = allChat[i]["type"] as Int
-            single["sender"] = fix(other = allChat[i]["sender"] as String)
-            single["data"] = fix(allChat[i]["data"] as ByteArray)
-//            println(single["data"])
-            allChatDecode += single
-            if (i % 20 == 0) {
-                progress = Progress(((i.toFloat() / allCount) * 500 + 200).toInt(), progress.msg)
+    private suspend fun chatsDecode(allChat: List<CodedChat>): ArrayList<Chat> {
+        return withContext(Dispatchers.Default) {
+            val allChatDecode = arrayListOf<Chat>()
+            val allCount = allChat.size
+            progress = Progress(progress.progress, "数据库解码...")
+            for (i in allChat.indices) {
+                val time = allChat[i].time
+                val type = allChat[i].type
+                val sender = fix(other = allChat[i].sender)
+                val data = fix(allChat[i].msg)
+//                println(single["data"])
+                allChatDecode += Chat(time, type, sender, data)
+                if (i % 20 == 0) {
+                    progress = Progress(((i.toFloat() / allCount) * 500 + 200).toInt(), progress.msg)
+                }
             }
+            allChatDecode.sortBy { it.time }
+            return@withContext allChatDecode
         }
-        allChatDecode.sortBy { it["time"] as Int }
-        return allChatDecode
     }
 
-    private fun toHtml(allChatDecode: ArrayList<HashMap<String, Any>>) {
-        progress = Progress(progress.progress, "导出网页...")
-        for (i in allChatDecode.indices) {
-            val item = allChatDecode[i]
-            saveStr += item["data"] as String
-            try {
-                htmlStr = "$htmlStr<font color=\"blue\">${getDateString(item["time"] as Int)}" +
-                        "</font>-----<font color=\"green\">${item["sender"] as String}</font>" +
-                        "</br>${htmlStrByType(item["type"] as Int) + item["data"] as String}</br></br>"
-            } catch (e: Exception) {
-                continue
+    private suspend fun toHtml(allChatDecode: ArrayList<Chat>) {
+        withContext(Dispatchers.Default) {
+            progress = Progress(progress.progress, "导出网页...")
+            var htmlStr = "<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /></head>"
+            for (i in allChatDecode.indices) {
+                val item = allChatDecode[i]
+                saveStr += item.msg
+                try {
+                    htmlStr = "$htmlStr<font color=\"blue\">${getDateString(item.time)}" +
+                            "</font>-----<font color=\"green\">${item.sender}</font>" +
+                            "</br>${htmlStrByType(item.type) + item.msg}</br></br>"
+                } catch (e: Exception) {
+                    continue
+                }
+                if (i % 20 == 0) {
+                    progress = Progress(((i.toFloat() / allChatDecode.size) * 250 + 700).toInt(), progress.msg)
+                }
             }
-            if (i % 20 == 0) {
-                progress = Progress(((i.toFloat() / allChatDecode.size) * 250 + 700).toInt(), progress.msg)
-            }
+            html = htmlStr
         }
     }
 
